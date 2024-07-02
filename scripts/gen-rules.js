bdaf3342c21c1bc14f3f28ccfd0c0277eeccaff8
lbb00 <mr@lbb00.com>
2024-07-03 02:45:15 +0800

    feat: use emmet css snippets

--- scripts/gen-rules.js
+++ scripts/gen-rules.js
@@ -1,21 +1,66 @@
-import EmmetCssSnippets from './emmet-css-snippet.json' assert { type: 'json' }
+import EmmetCssSnippets from './emmet-snippet/css.json' assert { type: 'json' }
+import EmmetCssCheatSheet from './emmet-snippet/cheat-sheet.json' assert { type: 'json' }
 import fs from 'node:fs'
+function genTemplateStringExpressionStr(content) {
+  return '${' + content + '}'
+}
+function genTemplateStringStr(content) {
+  return '`' + content + '`'
+}
+const result = [
+  ...Object.keys(EmmetCssSnippets)
+    .map((snippetKey) => {
+      const keys = snippetKey.split('|')
+      const content = EmmetCssSnippets[snippetKey]
+      return keys.map((key) => {
+        // TODO: {"bd": "border:${1:1px} ${2:solid} ${3:#000}"}
+        if (content.includes('${')) {
+          const result = ''
+          // return `[/^${key}(:|-)(.*)$/,(match) => ${result}]`
+          return undefined
+        }
 
-const result = Object.keys(EmmetCssSnippets)
-  .map((key) => {
-    if (key.includes('@')) return
-    if (EmmetCssSnippets[key].includes('${')) return
-    if (EmmetCssSnippets[key].includes('\\')) return
-    if (!EmmetCssSnippets[key].includes(':')) return
-    const cssProp = EmmetCssSnippets[key].split(':')[0]
-    const value = EmmetCssSnippets[key].split(':')[1]?.replace(';', '')
-    if (EmmetCssSnippets[key].includes('|')) {
-      return `[/^${key}-(.*)$/,(match) => ({"${cssProp}": \`${value.replace('|', '${match[1]}')}\`})]`
+        // TODO: {"ols": "outline-style:none|dotted|dashed|solid|double|groove|ridge|inset|outset"}
+        // I don't know how to handle this rule
+        // Use EmmetCssCheatSheet to generate rules
+        //
+        if (content.includes('|')) {
+          // const [cssProperty, cssValue] = content.split(':')
+          // const cssValues = cssVal.split('|')
+          // const result = genTemplateString(`${cssProperty}:${genTemplateStringExpression(
+          //   `?? match[2]`
+          // )}`)
+          // return `[/^${key}(:|-)(.*)$/,(match) => ${result}]`
+
+          return undefined
+        }
+
+        // {"p": "padding"}
+        if (!content.includes(':')) {
+          const result = `({"${content}":${genTemplateStringStr(genTemplateStringExpressionStr('match[2]'))}})`
+          return `[/^${key}(:|-)(.*)$/,(match) => ${result}]`
+        }
+
+        // unknown rule
+        return undefined
+      })
+    })
+    .flat(),
+  ...Object.keys(EmmetCssCheatSheet).map((cheatSheetKey) => {
+    const content = EmmetCssCheatSheet[cheatSheetKey]
+    const [cssProp, value] = content.split(':').map((i) => i.trim())
+    if (['|', '@', '${', '\\', '!'].every((i) => !content.includes(i))) {
+      return `[/^${cheatSheetKey}$/,() => ({"${cssProp}": \`${value.replace(';', '')}\`})]`
     }
-
-    return `[/^${key}$/,() => ({"${cssProp}": \`${value}\`})]`
-  })
-  .filter(Boolean)
-
-result.map((i) => console.log(i))
+    if (
+      content.includes('|') &&
+      Object.keys(EmmetCssSnippets).every((cssSnippet) => cssSnippet.split('|').every((key) => key !== cheatSheetKey))
+    ) {
+      const [cssProp, value] = content.split(':').map((i) => i.trim())
+      return `[/^${cheatSheetKey}-(.*)$/,(match) => ({"${cssProp}": \`${value
+        .replace(';', '')
+        .replace('|', '${match[1]}')}\`})]`
+    }
+  }),
+].filter(Boolean)
 fs.writeFileSync('src/rules.ts', `export default [\n${result.map((i) => i + ',').join('\n')}\n]`)
--- test/preset.test.ts
+++ test/preset.test.ts
@@ -6,14 +6,40 @@ const uno = createGenerator({
   presets: [presetEmmet()],
 })
 
-describe('test', () => {
-  it('foo', async () => {
-    const { css } = await uno.generate(['fz-16px', 'ai:c'].join(' '), {
+describe('test generate', () => {
+  it('fz-16px', async () => {
+    const { css } = await uno.generate('fz-16px', {
       preflights: false,
     })
     expect(css).toMatchInlineSnapshot(`
       "/* layer: default */
-      .fz-16px{font-size:16px;}
+      .fz-16px{font-size:16px;}"
+    `)
+  })
+  it('fsz-16px', async () => {
+    const { css } = await uno.generate('fsz-16px', {
+      preflights: false,
+    })
+    expect(css).toMatchInlineSnapshot(`
+      "/* layer: default */
+      .fsz-16px{font-size:16px;}"
+    `)
+  })
+  it('fsz:16px', async () => {
+    const { css } = await uno.generate('fsz-16px', {
+      preflights: false,
+    })
+    expect(css).toMatchInlineSnapshot(`
+      "/* layer: default */
+      .fsz-16px{font-size:16px;}"
+    `)
+  })
+  it('ai:c', async () => {
+    const { css } = await uno.generate('ai:c', {
+      preflights: false,
+    })
+    expect(css).toMatchInlineSnapshot(`
+      "/* layer: default */
       .ai\\:c{align-items:center;}"
     `)
   })
